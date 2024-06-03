a7b3a0fb015ac3916f5ad7ccb453451ad1076636
Rostyslav <roctik1996@gmail.com>
2024-06-03 21:16:47 +0300

    hotfix)

--- app/src/main/java/com/books/app/ui/view/library/LibraryScreen.kt
+++ app/src/main/java/com/books/app/ui/view/library/LibraryScreen.kt
@@ -17,6 +17,7 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBars
@@ -87,6 +88,7 @@ fun LibraryScreen(navController: NavHostController, viewModel: MainViewModel = k
             .background(Black)
             .padding(WindowInsets.statusBars.asPaddingValues())
             .padding(DefaultPadding)
+            .padding(WindowInsets.navigationBars.asPaddingValues())
     ) {
         Text(
             text = stringResource(R.string.library),
